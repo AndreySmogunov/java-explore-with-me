d16fdf37df9a6ef507b1ed3a710ad0aabe629ed3
AndreySmogunov <a.batman2003@yandex.ru>
2025-10-28 22:41:14 +0300

    Исправил pom.xml в stats-server и так же тесты

--- stats-server/src/test/java/stats/service/StatsServiceTest.java
+++ stats-server/src/test/java/stats/service/StatsServiceTest.java
@@ -2,7 +2,6 @@ package stats.service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import stats.dto.HitDto;
@@ -15,7 +14,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 @ActiveProfiles("test")
-@AutoConfigureMockMvc
 public class StatsServiceTest {
 
     @Autowired
@@ -25,17 +23,22 @@ public class StatsServiceTest {
     public void testSaveHit() {
         HitDto hitDto = new HitDto("app", "/uri", "192.168.0.1", LocalDateTime.now());
         HitDto savedHitDto = statsService.saveHit(hitDto);
-        assertEquals(hitDto.getApp(), savedHitDto.getApp());
-        assertEquals(hitDto.getUri(), savedHitDto.getUri());
-        assertEquals(hitDto.getIp(), savedHitDto.getIp());
+        assertEquals(hitDto.getApp(), savedHitDto.getApp(), "Название приложения должно совпадать");
+        assertEquals(hitDto.getUri(), savedHitDto.getUri(), "URI должен совпадать");
+        assertEquals(hitDto.getIp(), savedHitDto.getIp(), "IP-адрес должен совпадать");
     }
 
     @Test
     public void testGetStats() {
+        // Сначала сохраним хит, чтобы получить статистику
+        HitDto hitDto = new HitDto("app", "/uri", "192.168.0.1", LocalDateTime.now());
+        statsService.saveHit(hitDto);
+
         LocalDateTime start = LocalDateTime.now().minusDays(1);
         LocalDateTime end = LocalDateTime.now();
         List<String> uris = List.of("/uri");
         List<StatsDto> stats = statsService.getStats(start, end, uris, false);
-        assertEquals(1, stats.size());
+
+        assertEquals(1, stats.size(), "Ожидалась одна запись в статистике");
     }
 }
